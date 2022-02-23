1d53ee51b62fcdfc1c9cd375f1c8fb637356e9cd
Thiraphatrom <42799125+thiraphatrom@users.noreply.github.com>
2022-02-23 12:16:41 +0700

    Validation Form

---
+++ 23-2-2022/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+
+import { View, Image, TouchableOpacity } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import { createDrawerNavigator, DrawerContent } from '@react-navigation/drawer';
+
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+import ProductScreen from "./screens/ProductScreen";
+import DetailScreen from "./screens/DetailScreen";
+import MenuScreen from "./screens/MenuScreen";
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const NavigationDrawerStructure = (props) => {
+  //structure for navigation drawer
+  const toggleDrawer = () => {
+      props.navigationProps.toggleDrawer();
+  };
+  return (
+    <View style={{flexDirection:'row'}}>
+      <TouchableOpacity onPress={() => toggleDrawer()}>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function HomeScreenStack({navigation}) {
+  return (
+    <Stack.Navigator 
+        initialRouteName = 'HomeScreen' 
+        screenOptions = {{
+          headerStyle:{backgroundColor: 'skyblue'},
+          headerTintColor: 'white',
+          headerTitleStyle:{fontWeight: 'bold'},
+          headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation}/>,
+        }}
+      >
+        <Stack.Screen 
+          name='HomeScreen' 
+          component={HomeScreen} 
+          options={{title:'หน้าหลัก'}}
+        />
+        <Stack.Screen 
+          name='AboutScreen' 
+          component={AboutScreen} 
+          options={{title:'เกี่ยวกับ'}}
+        />
+        <Stack.Screen 
+          name='RegisterScreen' 
+          component={RegisterScreen} 
+          options={{title:'ลงทะเบียน'}}
+        />
+    </Stack.Navigator>
+  );
+}
+
+function ProductScreenStack({navigation}) {
+  return (
+    <Stack.Navigator 
+        initialRouteName = 'ProductScreen' 
+        screenOptions = {{
+          headerStyle:{backgroundColor: 'skyblue'},
+          headerTintColor: 'white',
+          headerTitleStyle:{fontWeight: 'bold'},
+          headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation}/>,
+        }}
+      >
+        <Stack.Screen 
+          name='ProductScreen' 
+          component={ProductScreen} 
+          options={{title:'สินค้า'}}
+        />
+        <Stack.Screen 
+          name='DetailScreen' 
+          component={DetailScreen} 
+          options={{title:'รายละเอียด'}}
+        />
+    </Stack.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator 
+        drawerContentOptions={{ activeTintColor:'pink', itemStyle:{marginVertical:5} }}
+        drawerContent = {(props)=><MenuScreen {...props}/>}
+      >
+        <Drawer.Screen 
+          name='HomeScreenStack' 
+          component={HomeScreenStack} 
+          options={{
+            drawerLabel:'HomeScreenStack'
+            }}
+          />
+        <Drawer.Screen name='ProductScreenStack' component={ProductScreenStack}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
